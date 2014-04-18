de6c40ce33bedf528d9f178a010bd08a5450ccdb
Tom Burns <tom.burns@jadedpixel.com>
2014-04-18 04:06:13 +0000

    go fmt

--- hmacauth.go
+++ hmacauth.go
@@ -1,67 +1,72 @@
 package hmacauth
 
 import (
-  "crypto/hmac"
-  "crypto/sha1"
-  "strings"
-  "errors"
-  "encoding/base64"
-  "encoding/hex"
-  "net/http"
-  "fmt"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 )
 
 type handler struct {
-  Key string
-  Param string
+	Key   string
+	Param string
 }
 
 type message struct {
-  message string
-  MAC string
-  Decoded string
+	message string
+	MAC     string
+	Decoded string
 }
 
 func parseToken(token string) (*message, error) {
-    parts := strings.Split(token, "--")
-    if len(parts) != 2 {
-       return nil, errors.New("Could not parse token into (message, MAC)")
-    }
+	parts := strings.Split(token, "--")
+	if len(parts) != 2 {
+		return nil, errors.New("Could not parse token into (message, MAC)")
+	}
 
-    decoded, err := base64.StdEncoding.DecodeString(parts[0])
-    if err != nil {
-       return nil, fmt.Errorf("Error decoding %s: %s", parts[0], err)
-    }
+	decoded, err := base64.StdEncoding.DecodeString(parts[0])
+	if err != nil {
+		return nil, fmt.Errorf("Error decoding %s: %s", parts[0], err)
+	}
 
-    return &message{message: parts[0], MAC: parts[1], Decoded: string(decoded)}, nil
+	return &message{message: parts[0], MAC: parts[1], Decoded: string(decoded)}, nil
 }
 
-func Authenticate(key string, param string, fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-    handler := &handler{Key: key, Param: param}
+func Authenticate(key string, param string, fn func(http.ResponseWriter, *http.Request, map[string]string)) http.HandlerFunc {
+	handler := &handler{Key: key, Param: param}
 
-    return func(w http.ResponseWriter, r *http.Request) {
-      err := r.ParseForm()
-      if err != nil {
-        fmt.Printf("Couldn't parse form from request")
-        http.Error(w, "", http.StatusUnauthorized)
-      }
-      token := r.Form[handler.Param]
-      if token == nil || len(token) != 1 {
-          fmt.Printf("Couldn't find param %v in request", handler.Param)
-          http.Error(w, "", http.StatusUnauthorized)
-      }
-      auth, err := parseToken(token[0])
-      if err != nil {
-        fmt.Printf("Couldn't parse form token")
-        http.Error(w, "", http.StatusUnauthorized)
-      }
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			log.Printf("Couldn't parse form from request")
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+		token := r.Form[handler.Param]
+		if token == nil || len(token) != 1 {
+			log.Printf("Couldn't find param %v in request", handler.Param)
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+		auth, err := parseToken(token[0])
+		if err != nil {
+			log.Printf("Couldn't parse form token")
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
 
-      if handler.isValid(auth) == false {
-          fmt.Printf("Token %v is invalid", token)
-          http.Error(w, "", http.StatusUnauthorized)
-      }
-      fn(w, r, auth.Decoded)
-    }
+		if handler.isValid(auth) == false {
+			log.Printf("Token %v is invalid", token)
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+		fn(w, r, auth.Decoded)
+	}
 }
 
 func (h *handler) isValid(message *message) bool {
@@ -70,6 +75,4 @@ func (h *handler) isValid(message *message) bool {
 	expectedMAC := hex.EncodeToString(mac.Sum(nil))
 
 	return hmac.Equal([]byte(message.MAC), []byte(expectedMAC))
-} 
-
-
+}
--- hmacauth_test.go
+++ hmacauth_test.go
@@ -1,63 +1,105 @@
 package hmacauth
 
 import (
-  "testing"
-  "net/http"
-  "net/http/httptest"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 var (
-  h = &handler{Key: "testkey", Param: "token"}
+	h = &handler{Key: "testkey", Param: "token"}
 )
 
 func TestParseToken(t *testing.T) {
-  message, err := parseToken("InRlc3Qi--3dd740af30f0453dd5220b56ba4fe57f48f892af")
-  if err != nil {
-    t.Fatalf("Error parsing token: %v", err)
-  }
+	message, err := parseToken("InRlc3Qi--3dd740af30f0453dd5220b56ba4fe57f48f892af")
+	if err != nil {
+		t.Fatalf("Error parsing token: %v", err)
+	}
 
-  if message.message != "InRlc3Qi" {
-    t.Fatalf("parseToken parsed message %v", message.message)
-  }
+	if message.message != "InRlc3Qi" {
+		t.Fatalf("parseToken parsed message %v", message.message)
+	}
 
-  if message.MAC != "3dd740af30f0453dd5220b56ba4fe57f48f892af" {
-    t.Fatalf("parseToken parsed MAC %v", message.MAC)
-  }
+	if message.MAC != "3dd740af30f0453dd5220b56ba4fe57f48f892af" {
+		t.Fatalf("parseToken parsed MAC %v", message.MAC)
+	}
 
-  if message.Decoded != "\"test\"" {
-    t.Fatalf("parseToken decoded message as %v", message.Decoded)
-  }
+	if message.Decoded != "\"test\"" {
+		t.Fatalf("parseToken decoded message as %v", message.Decoded)
+	}
 }
 
 func TestExpectedHMACValue(t *testing.T) {
-  message, err := parseToken("InRlc3Qi--3dd740af30f0453dd5220b56ba4fe57f48f892af")
+	message, err := parseToken("InRlc3Qi--3dd740af30f0453dd5220b56ba4fe57f48f892af")
 
-  if err != nil {
-    t.Fatalf("Error parsing token: %v", err)
-  }
+	if err != nil {
+		t.Fatalf("Error parsing token: %v", err)
+	}
 
-  if h.isValid(message) == false {
-    t.Fatalf("handler cannot decrypt ActiveSupport::messageVerifier tokens")
-  }
+	if h.isValid(message) == false {
+		t.Fatalf("handler cannot decrypt ActiveSupport::messageVerifier tokens")
+	}
 }
 
 func TestAuthenticateWrapperWorks(t *testing.T) {
-    handler := Authenticate("testkey", "token", func(response http.ResponseWriter, request *http.Request, token string) {
-      response.Write([]byte(token))
-    })
+	handler := Authenticate("testkey", "token", func(response http.ResponseWriter, request *http.Request, token string) {
+		response.Write([]byte(token))
+	})
 
-    recorder := httptest.NewRecorder()
-    url := "http://example.com/echo?token=InRlc3Qi--3dd740af30f0453dd5220b56ba4fe57f48f892af"
+	recorder := httptest.NewRecorder()
+	url := "http://example.com/echo?token=InRlc3Qi--3dd740af30f0453dd5220b56ba4fe57f48f892af"
 
-    req, err := http.NewRequest("GET", url, nil)
-    if err != nil {
-      t.Fatalf("err in http request: %v", err)
-    }
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("err in http request: %v", err)
+	}
 
-    handler(recorder, req)
+	handler(recorder, req)
 
-    if body := recorder.Body.String(); body != "\"test\"" {
-      t.Fatalf("unexpected body: %v", body)
-    }
+	if recorder.Code != 200 {
+		t.Fatalf("HMAC authentication failed")
+	}
+
+	if body := recorder.Body.String(); body != "\"test\"" {
+		t.Fatalf("unexpected body: %v", body)
+	}
 }
 
+func ConfirmFailURL(url string, t *testing.T) {
+	handler := Authenticate("testkey", "token", func(response http.ResponseWriter, request *http.Request, token string) {
+		t.Fatalf("HTTP request was not blocked in Authenticate()")
+	})
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("err in http request: %v", err)
+	}
+
+	handler(recorder, req)
+
+	if recorder.Code != 401 {
+		t.Fatalf("HMAC authentication failed to block bad MAC")
+	}
+}
+
+func TestAuthenticateBlocksWrongKey(t *testing.T) {
+	ConfirmFailURL("http://example.com/echo?token=InRlc3Qi--4dd740af30f0453dd5220b56ba4fe57f48f892af", t)
+}
+
+func TestAuthenticateBlocksNoKeyWithSplit(t *testing.T) {
+	ConfirmFailURL("http://example.com/echo?token=InRlc3Qi--", t)
+}
+
+func TestAuthenticateBlocksNoKeyWithoutSplit(t *testing.T) {
+	ConfirmFailURL("http://example.com/echo?token=InRlc3Qi", t)
+}
+
+func TestAuthenticateBlocksNoKeyWithJustSplit(t *testing.T) {
+	ConfirmFailURL("http://example.com/echo?token=--", t)
+}
+
+func TestAuthenticateBlocksNoKey(t *testing.T) {
+	ConfirmFailURL("http://example.com/echo", t)
+}
